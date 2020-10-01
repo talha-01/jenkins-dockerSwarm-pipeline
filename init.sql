d711166dcab7b13b7af94347a18db0535fa6d9bd
talha-01 <talhaclarus@gmail.com>
2020-10-01 08:32:24 -0700

    update

--- init.sql
+++ init.sql
@@ -7,6 +7,6 @@ CREATE TABLE phonebook_db.phonebook(
 
 INSERT INTO phonebook_db.phonebook (name, number)
     VALUES
-        ("Callahan", "1234567890"),
-        ("Sergio Taco", "67854"),
-        ("Vincenzo Altobelli", "876543554");
+        ("Jane Doe", "5559999687"),
+        ("John Smith", "5559997854"),
+        ("Talha", "5555554545");
